bc0f2cfea4450bbacb9f2db76fc37dd4df70fe05
thetechmarshall <marshalluzoma12@gmail.com>
2025-07-25 09:01:31 +0100

    page.tsx updated

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,21 +1,33 @@
+import { NextPage } from 'next';
 import CategoryProducts from "@/components/CategoryProducts";
 import Container from "@/components/Container";
 import Title from "@/components/Title";
 import { Category } from "@/sanity.types";
 import { getAllCategories } from "@/sanity/helpers/queries";
+import { notFound } from 'next/navigation';
 
 export const dynamicParams = true;
 
-const CategoryPage = async ({ params }: { params: { slug: string } }) => {
+interface Props {
+  params: { slug: string };
+}
+
+const CategoryPage: NextPage<Props> = async ({ params }) => {
+  const { slug } = params;
+
+  if (!slug) {
+    notFound();
+  }
+
   return (
     <Container className="py-10">
       <Title className="text-xl">Product Filter</Title>
-      <CategoryProducts slug={params.slug} />
+      <CategoryProducts slug={slug} />
     </Container>
   );
 };
 
-export async function generateStaticParams() {
+export async function generateStaticParams(): Promise<{ params: { slug: string } }[]> {
   const categories: Category[] = await getAllCategories();
 
   return categories
