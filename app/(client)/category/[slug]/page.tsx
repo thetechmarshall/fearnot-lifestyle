a27dcdc5bfa51d99828eb2b717cb3eba898260db
thetechmarshall <marshalluzoma12@gmail.com>
2025-07-30 07:47:07 +0100

    category page.tsx updated

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -2,13 +2,15 @@ import CategoryProducts from "@/components/CategoryProducts";
 import Container from "@/components/Container";
 import Title from "@/components/Title";
 
-const CategoryPage = async ({ params }: { params: { slug: string } }) => {
+export default async function CategoryPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   return (
     <Container className="py-10">
       <Title className="text-xl">Product Filter</Title>
       <CategoryProducts slug={params.slug} />
     </Container>
   );
-};
-
-export default CategoryPage;
+}
