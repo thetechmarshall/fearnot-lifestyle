5079c6a8b0ca8feb2b976f1ecdc7d85937c86a74
thetechmarshall <marshalluzoma12@gmail.com>
2025-07-30 15:24:59 +0100

    updated category/page.tsx and removed .next

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -1,20 +1,19 @@
-import CategoryProductsWrapper from "@/components/CategoryProductsWrapper"; 
+import CategoryProductsWrapper from "@/components/CategoryProductsWrapper";
 import Container from "@/components/Container";
 import Title from "@/components/Title";
 
-type CategoryPageProps = {
-  params: {
-    slug: string;
-  };
+
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-const CategoryPage = async ({ params }: CategoryPageProps) => {
+
+export default async function CategoryPage({ params }: Props) {
   return (
     <Container className="py-10">
       <Title className="text-xl">Product Filter</Title>
       <CategoryProductsWrapper slug={params.slug} />
     </Container>
   );
-};
-
-export default CategoryPage;
+}
