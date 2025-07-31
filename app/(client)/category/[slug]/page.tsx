5689c8e40de1d48eb728203dc30adb09dbb23d15
thetechmarshall <marshalluzoma12@gmail.com>
2025-07-31 22:16:50 +0100

    converted the params from object to a Promise

--- app/(client)/category/[slug]/page.tsx
+++ app/(client)/category/[slug]/page.tsx
@@ -2,18 +2,36 @@ import CategoryProductsWrapper from "@/components/CategoryProductsWrapper";
 import Container from "@/components/Container";
 import Title from "@/components/Title";
 
+// This is the implementation of the PageProp Promise type error fix.
+// It types the props object itself as containing a Promise.
+export default async function CategoryPage(props: {
+  params: Promise<{ slug: string }>;
+}) {
+  // We MUST await the promise to get the actual params object
+  const { slug } = await props.params;
 
-type Props = {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-
-export default async function CategoryPage({ params }: Props) {
   return (
     <Container className="py-10">
       <Title className="text-xl">Product Filter</Title>
-      <CategoryProductsWrapper slug={params.slug} />
+      <CategoryProductsWrapper slug={slug} />
     </Container>
   );
 }
+
+
+// Original code
+
+// import CategoryProducts from "@/components/CategoryProducts";
+// import Container from "@/components/Container";
+// import Title from "@/components/Title";
+
+// const CategoryPage = async ({ params }: { params: { slug: string } }) => {
+//   return (
+//     <Container className="py-10">
+//       <Title className="text-xl">Product Filter</Title>
+//       <CategoryProducts slug={params.slug} />
+//     </Container>
+//   );
+// };
+
+// export default CategoryPage;
